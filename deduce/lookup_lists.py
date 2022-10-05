6a3d963fbbbdd7a4b0b31e8f477ab93f1b12e93e
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2022-10-05 15:12:05 +0200

    Add Belgium institutions

--- deduce/lookup_lists.py
+++ deduce/lookup_lists.py
@@ -44,10 +44,11 @@ WHITELIST = list(
 
 # Read the list
 INSTITUTIONS = read_list("instellingen.lst", min_len=3)
+INSTITUTIONS += read_list("hospitals_be.lst", min_len=3)
 
 # These words sometimes occur as the first or final word of the official names of institutions,
 # but are not usually referred to as such in the colloquial version
-FILTER_VALUES = ["dr.", "der", "van", "de", "het", "'t", "in", "d'"]
+FILTER_VALUES = ["dr.", "der", "van", "de", "het", "'t", "in", "d'", "les"]
 
 # New list of institutions
 FILTERED_INSTITUTIONS = []
@@ -83,16 +84,48 @@ for institution in INSTITUTIONS:
     # "st", "st." and "ziekenhuis" have common abbreviations
     if "st" in institution:
         FILTERED_INSTITUTIONS.append(institution.replace("st ", "sint "))
+        FILTERED_INSTITUTIONS.append(institution.replace("st ", "saint "))
+        FILTERED_INSTITUTIONS.append(institution.replace("st ", "sainte "))
+        FILTERED_INSTITUTIONS.append(institution.replace("st ", "sint-"))
+        FILTERED_INSTITUTIONS.append(institution.replace("st ", "saint-"))
+        FILTERED_INSTITUTIONS.append(institution.replace("st ", "sainte-"))
 
     if "st." in institution:
         FILTERED_INSTITUTIONS.append(institution.replace("st. ", "sint "))
+        FILTERED_INSTITUTIONS.append(institution.replace("st. ", "saint "))
+        FILTERED_INSTITUTIONS.append(institution.replace("st. ", "sainte "))
+        FILTERED_INSTITUTIONS.append(institution.replace("st. ", "sint-"))
+        FILTERED_INSTITUTIONS.append(institution.replace("st. ", "saint-"))
+        FILTERED_INSTITUTIONS.append(institution.replace("st. ", "sainte-"))
 
     if "ziekenhuis" in institution:
         FILTERED_INSTITUTIONS.append(institution.replace("ziekenhuis", "zkh"))
+        FILTERED_INSTITUTIONS.append(institution.replace("ziekenhuis", ""))
+        FILTERED_INSTITUTIONS.append(institution.replace("ziekenhuis", "hopital"))
+        FILTERED_INSTITUTIONS.append(institution.replace("ziekenhuis", "kliniek"))
+        FILTERED_INSTITUTIONS.append(institution.replace("ziekenhuis", "clinique"))
+
+    if "hopital" in institution:
+        FILTERED_INSTITUTIONS.append(institution.replace("hopital", ""))
+        FILTERED_INSTITUTIONS.append(institution.replace("hopital", "clinique"))
+        FILTERED_INSTITUTIONS.append(institution.replace("hopital", "kliniek"))
+
+    if "clinique" in institution:
+        FILTERED_INSTITUTIONS.append(institution.replace("clinique", ""))
+        FILTERED_INSTITUTIONS.append(institution.replace("clinique", "hopital"))
+        FILTERED_INSTITUTIONS.append(institution.replace("clinique", "kliniek"))
+
+    if "kliniek" in institution:
+        FILTERED_INSTITUTIONS.append(institution.replace("kliniek", ""))
+        FILTERED_INSTITUTIONS.append(institution.replace("kliniek", "Clinique"))
 
     # If the institution name contains 3 or more words, also add the acronym
     if len(institution.split(" ")) >= 3:
-        FILTERED_INSTITUTIONS.append("".join(x[0] for x in institution.split(" ")))
+        institution = institution.replace("-", " ").replace("   ", " ").replace("  ", " ")
+        for x in institution.split(" "):
+            print(x)
+            print("".join(x[0]))
+        FILTERED_INSTITUTIONS.append("".join(x[0] for x in institution.replace("-", " ").split(" ")))
 
 # Remove doubles, occurrences on whitelist, and convert back to list
 INSTITUTIONS = list(set(FILTERED_INSTITUTIONS).difference(WHITELIST))
